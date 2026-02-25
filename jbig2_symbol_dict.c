1eb057fe46fdee615091a76e7b2695e8ca8c55bd
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-02-25 16:59:33 +0200

    Prevent integer overflow when accumulating total symbol count

--- jbig2_symbol_dict.c
+++ jbig2_symbol_dict.c
@@ -204,8 +204,14 @@ jbig2_sd_cat(Jbig2Ctx *ctx, uint32_t n_dicts, Jbig2SymbolDict **dicts)
 
     /* count the imported symbols and allocate a new array */
     symbols = 0;
-    for (i = 0; i < n_dicts; i++)
+    for (i = 0; i < n_dicts; i++) {
+        /* Prevent integer overflow when accumulating total symbol count */
+        if (UINT32_MAX - symbols < dicts[i]->n_symbols) {
+            jbig2_error(ctx, JBIG2_SEVERITY_FATAL, JBIG2_UNKNOWN_SEGMENT_NUMBER, "integer overflow when counting imported symbols");
+            return NULL;
+        }
         symbols += dicts[i]->n_symbols;
+    }
 
     /* fill a new array with new references to glyph pointers */
     new_dict = jbig2_sd_new(ctx, symbols);
