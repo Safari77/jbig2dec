e482361b6839fe225016d8695dc656de20e45933
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-02-25 11:11:00 +0200

    https://bugs.ghostscript.com/show_bug.cgi?id=708791

--- jbig2_arith_iaid.c
+++ jbig2_arith_iaid.c
@@ -47,7 +47,8 @@ jbig2_arith_iaid_ctx_new(Jbig2Ctx *ctx, uint8_t SBSYMCODELEN)
     Jbig2ArithIaidCtx *result;
     size_t ctx_size;
 
-    if (sizeof(ctx_size) * 8 <= SBSYMCODELEN)
+    /* Enforce an absolute maximum of 31 bits since p_result is int32_t */
+    if (SBSYMCODELEN > 31 || sizeof(ctx_size) * 8 <= SBSYMCODELEN)
     {
         jbig2_error(ctx, JBIG2_SEVERITY_FATAL, JBIG2_UNKNOWN_SEGMENT_NUMBER, "requested IAID arithmetic coding state size too large");
         return NULL;
@@ -82,7 +83,7 @@ jbig2_arith_iaid_decode(Jbig2Ctx *ctx, Jbig2ArithIaidCtx *actx, Jbig2ArithState 
     Jbig2ArithCx *IAIDx = actx->IAIDx;
     uint8_t SBSYMCODELEN = actx->SBSYMCODELEN;
     /* A.3 (1) */
-    int PREV = 1;
+    uint32_t PREV = 1;
     int D;
     int i;
 
@@ -97,7 +98,7 @@ jbig2_arith_iaid_decode(Jbig2Ctx *ctx, Jbig2ArithIaidCtx *actx, Jbig2ArithState 
         PREV = (PREV << 1) | D;
     }
     /* A.3 (3) */
-    PREV -= 1 << SBSYMCODELEN;
+    PREV -= 1U << SBSYMCODELEN;
 #ifdef VERBOSE
     fprintf(stderr, "IAID result: %d\n", PREV);
 #endif
