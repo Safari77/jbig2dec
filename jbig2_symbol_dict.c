15a3ec44a266e93a9a28ab3544c6f484505e08cf
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2026-06-06 19:50:49 +0300

    rebase b055f6b759865c5b04f7b0e759c8a91be997d0b4
    I already had the fix, but rebase so future patches apply without errors

--- jbig2_symbol_dict.c
+++ jbig2_symbol_dict.c
@@ -212,10 +212,11 @@ jbig2_sd_cat(Jbig2Ctx *ctx, uint32_t n_dicts, Jbig2SymbolDict **dicts)
 
     /* count the imported symbols and allocate a new array */
     symbols = 0;
-    for (i = 0; i < n_dicts; i++) {
-        /* Prevent integer overflow when accumulating total symbol count */
-        if (UINT32_MAX - symbols < dicts[i]->n_symbols) {
-            jbig2_error(ctx, JBIG2_SEVERITY_FATAL, JBIG2_UNKNOWN_SEGMENT_NUMBER, "integer overflow when counting imported symbols");
+    for (i = 0; i < n_dicts; i++)
+    {
+        if (dicts[i]->n_symbols > UINT32_MAX - symbols)
+        {
+            jbig2_error(ctx, JBIG2_SEVERITY_FATAL, JBIG2_UNKNOWN_SEGMENT_NUMBER, "too many symbols in dicts to concat");
             return NULL;
         }
         symbols += dicts[i]->n_symbols;
